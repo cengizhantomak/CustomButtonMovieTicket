30ce876624a1d6a4e3329b7cd1330bf90a890d97
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-08-01 12:29:03 +0300

    added RedButton

--- Sources/CustomButton/RedButton.swift
+++ Sources/CustomButton/RedButton.swift
@@ -1,2 +1,29 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+public class RedButton: UIButton {
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtonUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtonUI()
+    }
+
+    public func setupButtonText(text: String) {
+        setTitle(text, for: .normal)
+    }
+
+    func setupButtonUI() {
+        layer.cornerRadius = 4
+        backgroundColor = UIColor(named: "buttonRed")
+
+        titleLabel?.font = UIFont(name: "SFProText-Semibold", size: 16)
+        setTitleColor(UIColor.white, for: .normal)
+
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: 48).isActive = true
+    }
+}
